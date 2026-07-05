50a85696dc117378bd401b687449f877a91c3c35
Shern Ren Tee <shernren@gmail.com>
2026-07-05 21:11:32 +1000

    Initial creation with MDAnalysis Cookiecutter 0.1

---
+++ qdiffusivity/__init__.py
@@ -0,0 +1,9 @@
+"""
+qdiffusivity
+Quantile-based estimation of local diffusivities in molecular dynamics simulations of nanoconfined liquids
+"""
+
+# Add imports here
+from importlib.metadata import version
+
+__version__ = version("qdiffusivity")
